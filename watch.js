b847d4649caad89dd48860c2ca0240755ab6e8e5
Nic Schumann <nic@nicschumann.co>
2021-03-03 09:12:52 -0500

    [dev] upgrades esbuild and replaces dev server with esbuild-native server

--- watch.js
+++ watch.js
@@ -1,10 +1,14 @@
 const chokidar  = require('chokidar');
 const esbuild   = require('esbuild');
 
-const buildjs = async path => {
-  console.log('changed', path);
-  let s = Date.now();
-  esbuild.build({
+// refactoring watch process into esbuild entirely to avoid
+// the extra chokidar dependency.
+
+esbuild.serve(
+  {
+    servedir: 'dst'
+  },
+  {
     entryPoints: ['src/main.js'],
     bundle: true,
 
@@ -15,32 +19,56 @@ const buildjs = async path => {
     loader: {'.vs': 'text', '.fs': 'text'},
 
     platform: 'node',
+    // why is it bundling for node, not for browsers?
     target: ['node10.4'],
-    outfile: 'dst/bundle.js'
-
-  })
-  .then(() => {
-    let e = Date.now();
-    console.log(`No Errors. Build completed (${(e - s) / 1000}s).\n`);
-  })
-  .catch(() => {
-    let e = Date.now();
-    console.log(`Errors. Build failed (${(e - s) / 1000}s).\n`);
+    outdir: 'dst'
+  }).then(server => {
+    console.log(`Serving on ${server.host}:${server.port}.`);
+    console.log(`Rebuild from the browser with ⌘-R.`);
+    console.log(`Stop this server with ctrl-C.\n`);
   });
-};
 
 
-const jswatcher = chokidar.watch('src/**/*.*', {
-  ignored: "src/**/bundle*",
-  persistent: true
-});
-
-
-jswatcher
-  .on('ready', async () => {
-    buildjs('all');
-
-    jswatcher
-      .on('add', buildjs)
-      .on('change', buildjs);
-  });
+// const buildjs = async path => {
+//   console.log('changed', path);
+//   let s = Date.now();
+//   esbuild.build({
+//     entryPoints: ['src/main.js'],
+//     bundle: true,
+//
+//     minify: false,
+//     sourcemap: true,
+//
+//     // Load shaders as text for WebGL.
+//     loader: {'.vs': 'text', '.fs': 'text'},
+//
+//     platform: 'node',
+//     target: ['node10.4'],
+//     outfile: 'dst/bundle.js'
+//
+//   })
+//   .then(() => {
+//     let e = Date.now();
+//     console.log(`No Errors. Build completed (${(e - s) / 1000}s).\n`);
+//   })
+//   .catch(() => {
+//     let e = Date.now();
+//     console.log(`Errors. Build failed (${(e - s) / 1000}s).\n`);
+//   });
+// };
+//
+//
+// const jswatcher = chokidar.watch('src/**/*.*', {
+//   ignored: "src/**/bundle*",
+//   persistent: true
+// });
+//
+//
+// jswatcher
+//   .on('ready', async () => {
+//     buildjs('all');
+//
+//     jswatcher
+//       .on('add', buildjs)
+//       .on('change', buildjs);
+//   });
