6de9a7af975d9bce09eb506cac7729b5d31acfeb
Nic Schumann <nic@nicschumann.co>
2021-03-10 17:19:42 -0500

    [shaders] adds shaders for drawing crisp edges in color mode

--- src/fluid-shaders/add-color.fs
+++ src/fluid-shaders/add-color.fs
@@ -10,10 +10,17 @@ uniform vec2 uOrigin;
 uniform vec4 uColor;
 uniform float uRadius;
 
+#define CLAMPED_COLORS
+
 void main ()
 {
   vec2 p = vUv - uOrigin;
   vec4 dir = exp(-dot(p, p) / uRadius) * uColor;
   vec4 base = texture2D(uSource, vUv);
+
+#ifdef CLAMPED_COLORS
+  gl_FragColor = clamp(base + dir, 0.0, 1.0);
+#else
   gl_FragColor = base + dir;
+#endif
 }
